b6ef04a5dec3dfe375af530a073b340cddf7de97
Declan Dunne <d.dunne@ucc.ie>
2017-07-11 16:50:32 +0100

    Reverting back to original license.py file. Instead listing licenses via the licenses_group_url option in the CKAN configuration file

--- ckan/model/license.py
+++ ckan/model/license.py
@@ -93,22 +93,21 @@ class LicenseRegister(object):
             self.load_licenses(group_url)
         else:
             default_license_list = [
-#                LicenseNotSpecified(),
-#                LicenseOpenDataCommonsPDDL(),
-#                LicenseOpenDataCommonsOpenDatabase(),
-#                LicenseOpenDataAttribution(),
+                LicenseNotSpecified(),
+                LicenseOpenDataCommonsPDDL(),
+                LicenseOpenDataCommonsOpenDatabase(),
+                LicenseOpenDataAttribution(),
                 LicenseCreativeCommonsZero(),
                 LicenseCreativeCommonsAttribution(),
                 LicenseCreativeCommonsAttributionShareAlike(),
-#                LicenseGNUFreeDocument(),
-#                LicenseOtherOpen(),
-#                LicenseOtherPublicDomain(),
-#                LicenseOtherAttribution(),
-#                LicenseOpenGovernment(),
+                LicenseGNUFreeDocument(),
+                LicenseOtherOpen(),
+                LicenseOtherPublicDomain(),
+                LicenseOtherAttribution(),
+                LicenseOpenGovernment(),
                 LicenseCreativeCommonsNonCommercial(),
-                LicenseCreativeCommonsNonCommercialShareAlike(),
-#                LicenseOtherNonCommercial(),
-#                LicenseOtherClosed(),
+                LicenseOtherNonCommercial(),
+                LicenseOtherClosed(),
                 ]
             self._create_license_list(default_license_list)
 
@@ -333,19 +332,11 @@ class LicenseOpenGovernment(DefaultLicense):
 
 class LicenseCreativeCommonsNonCommercial(DefaultLicense):
     id = "cc-nc"
-    url = "http://creativecommons.org/licenses/by-nc/4.0/"
+    url = "http://creativecommons.org/licenses/by-nc/2.0/"
 
     @property
     def title(self):
         return _("Creative Commons Non-Commercial (Any)")
-
-class LicenseCreativeCommonsNonCommercialShareAlike(DefaultLicense):
-    id = "cc-nc-sa"
-    url = "http://creativecommons.org/licenses/by-nc-sa/4.0/"
-
-    @property
-    def title(self):
-        return _("Creative Commons Non-Commercial Share-Alike")
 
 class LicenseOtherNonCommercial(DefaultLicense):
     id = "other-nc"
